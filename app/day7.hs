86c3621b9e43515147b0bc9ac5b05b203ecbd64c
Jeffrey Becca <jeff.becca@protonmail.com>
2023-02-09 19:38:22 -0500

    refactored to use all Text instead of String

--- app/day7.hs
+++ app/day7.hs
@@ -1,29 +1,27 @@
 module Main where
-
-import Data.Text (Text)
 import qualified Data.Text.IO as TIO
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import Text.Megaparsec.Error
 import qualified Text.Megaparsec.Char.Lexer as L
 import qualified Data.Map as Map
 import Data.Void
-import Data.List.Split
-import Data.List (intercalate, foldl')
+import Data.Char (isPrint)
+import Data.Text (Text, splitOn, intercalate)
+import Data.List (foldl')
 
 main :: IO ()
 main = do
-    inputString <- TIO.readFile "input/day7.txt"
-    case parse pInput "" inputString of
+    inputText <- TIO.readFile "input/day7_reduced.txt"
+    case parse pInput "" inputText of
       Left e ->  print (errorBundlePretty e) --print ("parse error" ++ show e)
-      Right y -> print y
+      Right y -> print $ reverse $ Map.toList $ snd (foldl' itemToMap (Path "", Map.empty) y)
 
 type Parser = Parsec Void Text
 
-data StructuredInput = ChangeDir String
+data StructuredInput = ChangeDir Text
                      | LsDir
-                     | Dir String
-                     | File Integer -- also has a name, but we dont need it
+                     | Dir Text
+                     | File Integer Text
                      deriving Show
 
 pInteger :: Parser Integer
@@ -39,17 +37,30 @@ pLine =
   pDir <|>
   pFile
 
+
 pChangeDir :: Parser StructuredInput
-pChangeDir = ChangeDir <$> ("$ cd " *> many printChar)
+--pChangeDir = ChangeDir <$> ("$ cd " *> many printChar)
+pChangeDir = do
+  _   <- Text.Megaparsec.chunk "$ cd "
+  dir <- takeWhile1P (Just "print character") isPrint
+  return (ChangeDir dir)
 
 pLsDir :: Parser StructuredInput
 pLsDir = LsDir <$ "$ ls"
 
 pDir :: Parser StructuredInput
-pDir = Dir <$> ("dir " *> many letterChar)
+--pDir = Dir <$> ("dir " *> many letterChar)
+pDir = do
+  _ <- Text.Megaparsec.chunk "dir "
+  dir <- takeWhile1P (Just "print char") isPrint
+  return (Dir dir)
 
 pFile :: Parser StructuredInput
-pFile = File <$> (pInteger <* many printChar) -- <*> (many letterChar)
+--pFile = File <$> (pInteger) <*> (many (letterChar <|> punctuationChar))-- <* many printChar) -- <*> (many letterChar)
+pFile = do
+  size <- pInteger
+  fname <- takeWhile1P (Just "p char") isPrint
+  return (File size fname)
 
 sc :: Parser ()
 sc = L.space space1 empty empty
@@ -57,20 +68,35 @@ sc = L.space space1 empty empty
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
 
-type Path = String
-type DirOrFileName = String
-type Size = Integer
+newtype Path = Path Text deriving (Eq,Ord,Show)
+newtype DirOrFileName = DirOrFileName Text deriving (Eq,Ord,Show)
+newtype Size = Size Integer deriving (Eq, Ord, Show)
 
-type InputMap = Map.Map Path (DirOrFileName, Size)
+type InputMap = Map.Map Path [(DirOrFileName, Size)]
 
-fChangeDir :: Path -> String -> Path
-fChangeDir p ".." = intercalate "/" $ drop 2 $ splitOn "/" p
-fChangeDir p "/"    = "/"
-fChangeDir p s    = "/" <> s <> p
+fChangeDir :: Path -> Text -> Path
+fChangeDir (Path p) ".." = Path $ "/" <> intercalate "/" (drop 2 $ splitOn "/" p)
+fChangeDir (Path _) "/"    = Path "/"
+fChangeDir (Path p) s    = Path $ "/" <> s <> p
+
+addDirToValue :: Path -> InputMap -> DirOrFileName -> Size -> InputMap
+-- need to check if value is in Map already or not. Insert if not
+addDirToValue path m dir size
+  | Map.member path m == True = Map.adjust ([(dir, size)] <>) path m
+  | Map.member path m == False = Map.insert path [(dir, size)] m
+  | otherwise = m
+
 itemToMap :: (Path, InputMap) -> StructuredInput -> (Path, InputMap)
-itemToMap (s, answerMap) input =
+itemToMap (Path s, answerMap) input =
   case input of
-    (ChangeDir dir) -> ((fChangeDir s dir), answerMap)
-    (LsDir)         -> (s, answerMap)
-    (Dir dir)       -> (s, answerMap)
-    (File size)     -> (s, Map.insert s (s, size) answerMap)
+    (ChangeDir dir)   -> (fChangeDir (Path s) dir, answerMap)
+    LsDir             -> (Path s, answerMap)
+    (Dir dir)         -> (Path s, addDirToValue (Path s) answerMap (DirOrFileName $ dir <> s) (Size 0))
+    (File size name)  -> (Path s, addDirToValue (Path s) answerMap (DirOrFileName $ name <> s) (Size size))
+
+type SizeMap = Map.Map Path Size
+
+-- do another foldl' here to get a final map
+sizeOfDirectory :: (Path, [(DirOrFileName, Size)]) -> SizeMap
+sizeOfDirectory = undefined
+  -- where list = Map.reverse $ Map.toList inputMap
